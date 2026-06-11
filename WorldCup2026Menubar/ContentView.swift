0e4b084cc1c157d8f0db9e8bdf0d5982ca7c4d61
Gilbert Pellegrom <gilbert@pellegrom.me>
2026-06-11 22:34:13 +0100

    Initial Commit

---
+++ WorldCup2026Menubar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorldCup2026Menubar
+//
+//  Created by Gilbert Pellegrom on 11/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorldCup2026Menubar/WorldCup2026MenubarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldCup2026MenubarApp.swift
+//  WorldCup2026Menubar
+//
+//  Created by Gilbert Pellegrom on 11/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct WorldCup2026MenubarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
